f4edf9baa061c89c135a41aef73b45a118a3c3a5
SergeiBas <sergei.bas06@gmail.com>
2023-09-29 16:40:39 +0300

    Finish modul16

--- src/test/java/goit/MODUL12javaDev/Modul12JavaDevApplicationTests.java
+++ src/test/java/goit/MODUL12javaDev/Modul12JavaDevApplicationTests.java
@@ -3,11 +3,13 @@ package goit.MODUL12javaDev;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Modul12JavaDevApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void someTest() {
+		assertEquals(1,1);
 	}
-
 }
